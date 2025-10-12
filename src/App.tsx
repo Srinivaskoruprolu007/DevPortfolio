1dc34624679231a67a2d4fd15db7951277173b7b
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-10-12 19:39:38 +0530

    refactor(ui): comment out Testimonials component
    - Disabled the Testimonials section in the main app render
    - Kept the component code intact for potential future use
    - Improved UI focus by removing Testimonials temporarily

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ function App() {
           <About />
           <Skills />
           <Projects />
-          <Testimonials />
+          {/* <Testimonials /> */}
           <Contact />
         </main>
 
